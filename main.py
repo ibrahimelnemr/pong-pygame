ead5568566a5489cb0445333d01116aeb454e0b6
Ibrahim El Nemr <70034333+iwelnemr@users.noreply.github.com>
2021-01-31 21:09:03 +0200

    Initialize game

---
+++ main.py
@@ -0,0 +1,41 @@
+import math
+import random
+
+import pygame
+from pygame import mixer
+
+pygame.init()
+
+screen = pygame.display.set_mode((800,600))
+
+font = pygame.font.Font('Nexa Light.otf', 50)
+
+text = font.render('Welcome to Pong', True, (255,255,255))
+
+textRect = text.get_rect()
+
+ballX = 0
+ballY = 0
+
+ballX_change = 300
+bally_change = 300
+
+ball_radius = 20
+
+ball_color = (50,135, 255)
+
+running = True
+while running:
+    screen.fill((0,0,0))
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_q:
+                running = False
+
+
+    screen.blit(text,textRect)
+    pygame.draw.circle(screen, ball_color, (ballX,ballY), ball_radius,0)
+    pygame.display.update()
