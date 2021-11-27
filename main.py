2874cbf26f1c50992261f15f883347c2c56c4e45
Ibrahim El Nemr <70034333+iwelnemr@users.noreply.github.com>
2021-11-27 14:31:03 +0200

    Update main.py

--- main.py
+++ main.py
@@ -14,18 +14,21 @@ text = font.render('Welcome to Pong', True, (255,255,255))
 
 textRect = text.get_rect()
 
-ballX = 0
-ballY = 0
+ballX = 400
+ballY = 300
 
-ballX_change = 300
-bally_change = 300
+ballX_change = 1
+ballY_change = 1
 
 ball_radius = 20
 
 ball_color = (50,135, 255)
 
 def ball_move():
-    if 
+    global ballX, ballY, ballX_change, ballY_change
+    ballX = ballX + ballX_change
+    ballY = ballY + ballY_change
+
 
 running = True
 while running:
@@ -37,8 +40,7 @@ while running:
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_q:
                 running = False
-
-    
     screen.blit(text,textRect)
+    ball_move()
     pygame.draw.circle(screen, ball_color, (ballX,ballY), ball_radius,0)
     pygame.display.update()
